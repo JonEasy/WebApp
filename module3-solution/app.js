3935b8d06c534d498c4172b231dfd4d49a2fe539
Jonel <jonel.palouvisa@ge.com>
2020-07-04 14:43:14 +0200

    retriving server but not shoing found items

---
+++ module3-solution/app.js
@@ -0,0 +1,78 @@
+(function() {
+'use strict';
+
+angular.module("NarrowItDownApp", [])
+    .service("MenuSearchService", MenuSearchService)
+    .controller("NarrowItDownController", NarrowItDownController)
+    .constant("ApiBasePath", "https://davids-restaurant.herokuapp.com/menu_items.json")
+    .directive('foundItems', foundItemsDirective);
+
+
+
+function foundItemsDirective(){
+
+    var ddo = {
+        restrict: "E",
+        templateUrl: 'foundItemsList.html',
+        scope: {
+            foundItems: '<found',
+            onRemove: '&'
+        },
+        controller: FoundItemsDirectiveController,
+        bindToController: true,
+        controllerAs: "ctrller"
+    };
+
+    return ddo;
+}
+
+function FoundItemsDirectiveController(){}
+
+
+NarrowItDownController.$inject = ["MenuSearchService"];
+function NarrowItDownController(MenuSearchService) {
+
+    var ctrl = this;
+    ctrl.searchTerm = '';
+    ctrl.found = []
+    ctrl.getMatchedMenuItems = function () {
+        console.log("SearchTerm seen in ctrl: ", ctrl.searchTerm );
+        ctrl.found = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm )
+
+    console.log("ctrl.found as seen at ctrl: ", ctrl.found);
+    }
+    ctrl.removeItem = function(itemIndex){
+        ctrl.found.splice(itemIndex,1)
+    }
+};
+
+
+
+MenuSearchService.$inject = ["$http", "ApiBasePath"];
+function MenuSearchService($http, ApiBasePath) {
+    var service = this
+    var items = [];
+    service.getMatchedMenuItems = function (searchTerm) {
+
+        items= [];
+        searchTerm = searchTerm.trim().toLowerCase()
+
+        $http({
+            method: "GET",
+            url: (ApiBasePath)
+        }).then(function (response){
+            for ( var i = 0 ; i < response.data.menu_items.length ; i++ ) {
+                if ( response.data.menu_items[i].name.toLowerCase().indexOf(searchTerm) !== -1 ) {
+                    items.push( response.data.menu_items[i] );
+                }
+            }
+        }).catch(function (error) {
+            console.log("Error while retrieving the data.")
+        });
+
+        return items
+    };
+
+}
+
+})();
